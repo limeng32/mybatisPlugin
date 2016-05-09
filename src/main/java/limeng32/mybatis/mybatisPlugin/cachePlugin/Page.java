4c4848cc7c0b3e993a013d034b998dfa1e10ffb2
limeng32 <limeng32@live.cn>
2016-05-09 15:11:37 +0800

    Add Page and be 1.1.1

---
+++ src/main/java/limeng32/mybatis/mybatisPlugin/cachePlugin/Page.java
@@ -0,0 +1,43 @@
+package limeng32.mybatis.mybatisPlugin.cachePlugin;
+
+import java.util.Collection;
+
+public class Page<T> {
+
+	private int pageNo;
+
+	private int maxPageNum;
+
+	private Collection<T> pageItems;
+
+	public Page(Collection<T> items, Limitable limitable) {
+		pageItems = items;
+		pageNo = limitable.getPageNo();
+		maxPageNum = limitable.getMaxPageNum();
+	}
+
+	public int getPageNo() {
+		return pageNo;
+	}
+
+	public void setPageNo(int pageNo) {
+		this.pageNo = pageNo;
+	}
+
+	public int getMaxPageNum() {
+		return maxPageNum;
+	}
+
+	public void setMaxPageNum(int maxPageNum) {
+		this.maxPageNum = maxPageNum;
+	}
+
+	public Collection<T> getPageItems() {
+		return pageItems;
+	}
+
+	public void setPageItems(Collection<T> pageItems) {
+		this.pageItems = pageItems;
+	}
+
+}
